b6ed46a863032b8321e5e739fb07407a04ab94da
odybgs <ody.assistindo@gmail.com>
2024-07-19 18:29:56 +0700

    first comm

---
+++ src/main/java/com/obs/uas_pbo/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.obs.uas_pbo;
+
+import com.obs.uas_pbo.noteapp.NoteAppMenu;
+
+/**
+ *
+ * @author mirage
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        NoteAppMenu noteapp = new NoteAppMenu("notes.db");
+        noteapp.start();
+    }
+}
